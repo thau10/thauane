6691cee50a04fb46c852b4d7b02f79e828dd30b4
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-02-28 09:08:11 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,12 @@
+#tabuada
+numero= int(input('digite um numero para exibir a tabuada '))
+multiplicador= 1
+
+while(multiplicador<=10):
+    resultado = numero * multiplicador
+    print(f'{numero} x {multiplicador} = {resultado}')
+    
+    multiplicador = multiplicador +1
+
+
+
---
+++ thauane.py
@@ -0,0 +1,9 @@
+#contador
+
+contador=5
+while(contador > 0):
+    print('contador')
+
+    contador = contador -1
+
+print('fim')
